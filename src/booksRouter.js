ffbd2137b61478299ff7fa219be827a1b61d9df2
andreacolina <a.colina.2020@alumnos.urjc.es>
2020-12-12 00:56:21 +0100

    Added commentaries functionality

--- src/booksRouter.js
+++ src/booksRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
-// import {addComment, getComments} from "./commentsService.js";
 
 const url = require("url");
 const router = express.Router();
 
 const books = new Map();
+const comments = new Map();
 let nextId = 1;
+let nextCommentId = 1;
 
 addBook({
     title: 'Don Quijote de la Mancha',
@@ -22,6 +23,18 @@ addBook({
     comments: []
 })
 
+addComment({
+    author: 'Andrea',
+    text: 'Me ha gustado mucho este libro.',
+    score: 5
+}, 1);
+
+addComment({
+    author: 'Juanma',
+    text: 'Me ha parecido un rollo terrible.',
+    score: 0
+}, 2);
+
 //GET books
 router.get('/books', ((req, res) =>
         res.json([...books.values()])
@@ -32,10 +45,10 @@ router.get('/books/:id', ((req, res) => {
         const id = req.params.id;
         const book = books.get(id);
 
-        if(!book) {
+        if (!book) {
             res.sendStatus(404);
         } else {
-            // book.comments = getComments(book.id)
+            getComments(book.id)
             res.json(book);
         }
     }
@@ -58,18 +71,74 @@ router.post('/books', (req, res) => {
     }
 });
 
+//DELETE book at id -> Creo que esta operacion no se contemplaba en la otra practica.
 router.delete('/books/:id', (req, res) => {
     const id = req.params.id;
     const book = books.get(id);
 
-    if(!book) {
+    if (!book) {
         res.sendStatus(404);
     } else {
-        if(books.delete(id)) {
+        if (books.delete(id)) {
             res.sendStatus(200);
         } else {
             res.sendStatus(409)
         }
+    }
+});
+
+//GET comments
+router.get('/comments', ((req, res) =>
+        res.json([...comments.values()])
+));
+
+//GET comment by its id
+router.get('/comments/:id', ((req, res) => {
+        const id = req.params.id;
+        const comment = comments.get(id);
+
+        if (!comment) {
+            res.sendStatus(404);
+        } else {
+            res.json(comment);
+        }
+    }
+));
+
+//DELETE comment by its id
+router.delete('/comments/:id', (req, res) => {
+    const id = req.params.id;
+    const comment = comments.get(id);
+
+    if (!comment) {
+        res.sendStatus(404);
+    } else {
+        if (comments.delete(id)) {
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(409)
+        }
+    }
+});
+
+//POST comment
+router.post('/comments/:bookId', (req, res) => {
+    if (books.has(req.params.bookId)) {
+        if (!validComment(req.body)) {
+            res.sendStatus(400);
+        } else {
+            let comment = {
+                author: req.body.author,
+                text: req.body.text,
+                score: req.body.score,
+            };
+
+            addComment(comment,req.params.bookId);
+            res.location(fullUrl(req));
+            res.json(comment);
+        }
+    } else {
+        res.sendStatus(400);
     }
 });
 
@@ -83,6 +152,10 @@ function validBook(book) {
     return typeof book.title == 'string' && typeof book.summary == 'string' && typeof book.author == 'string';
 }
 
+function validComment(comment) {
+    return typeof comment.author == 'string' && typeof comment.text == 'string' && typeof comment.score == 'number';
+}
+
 function fullUrl(req) {
     const fullUrl = url.format({
         protocol: req.protocol,
@@ -91,6 +164,24 @@ function fullUrl(req) {
     });
     return fullUrl +
         (fullUrl.endsWith('/') ? '' : '/');
+}
+
+// Comments:
+function addComment(comment, bookId) {
+    let id = nextCommentId++;
+    comment.id = id.toString();
+    comment.bookId = bookId.toString();
+    comments.set(comment.id, comment);
+}
+
+
+function getComments(bookId) {
+    books.get(bookId).comments = [];
+    comments.forEach((comment) => {
+        if (comment.bookId === bookId) {
+            books.get(bookId).comments.push(comment);
+        }
+    });
 }
 
 module.exports = router;
